fcb74ee4bfd9b9b2a7fb6a58495f657306bf2780
dauid64 <carlosdauid64br@gmail.com>
2024-01-02 17:04:00 -0300

    Add Lógica de criar lista de portas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Porta from "../components/Porta";
 import Presente from "../components/Presente";
+import { atualizarPortas, criarPortas } from "../functions/portas";
 import PortaModel from "../model/porta"
 import { useState } from "react";
 
 export default function Home() {
-  const [p1, setP1] = useState(new PortaModel(1))
+  const [portas, setPortas] = useState(criarPortas(3, 2))
+
+  function renderizarPortas() {
+    return portas.map(porta => {
+      return (
+        <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}></Porta>
+      )
+    })
+  }
   return (
-    <div>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}></Porta>
+    <div style={{display: "flex"}}>
+      {renderizarPortas()}
     </div>
   )
 }
