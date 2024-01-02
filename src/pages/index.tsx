d6876a64cfb1588e0d57e88fc5ec3e34a6b5d20e
dauid64 <carlosdauid64br@gmail.com>
2024-01-02 16:32:35 -0300

    selecao da porta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1))
   return (
     <div>
-      <Porta value={p1}></Porta>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}></Porta>
     </div>
   )
 }
